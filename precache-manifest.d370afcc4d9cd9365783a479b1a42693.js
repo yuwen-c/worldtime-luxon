aadcc8f0f78889dae652d2b319bdaee4c18fc663
yuwen-c <yuwen.azulejos@gmail.com>
2020-12-22 16:56:56 +0800

    Updates

---
+++ precache-manifest.d370afcc4d9cd9365783a479b1a42693.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b58011f7f2c0e44cafa5dc06e6b68c27",
+    "url": "/worldtime-luxon/index.html"
+  },
+  {
+    "revision": "f224ab5a96a4f1765c26",
+    "url": "/worldtime-luxon/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "7549b5129f2afb27183c",
+    "url": "/worldtime-luxon/static/css/main.2c0f2b34.chunk.css"
+  },
+  {
+    "revision": "f224ab5a96a4f1765c26",
+    "url": "/worldtime-luxon/static/js/2.672bbcfe.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/worldtime-luxon/static/js/2.672bbcfe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7549b5129f2afb27183c",
+    "url": "/worldtime-luxon/static/js/main.ce1ba5ea.chunk.js"
+  },
+  {
+    "revision": "95c98e4f897cc1cdfb07",
+    "url": "/worldtime-luxon/static/js/runtime-main.eafbcf8a.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/worldtime-luxon/precache-manifest.d370afcc4d9cd9365783a479b1a42693.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/worldtime-luxon/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
